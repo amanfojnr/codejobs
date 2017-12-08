4bed3821083a6d34cdff901e36decad9789dea85
amanfojnr <kenmarch@zoho.com>
2017-12-08 14:47:46 +0000

    added staticfiles config

--- codejobs/settings.py
+++ codejobs/settings.py
@@ -127,8 +127,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
+# Extra places for collectstatic to find static files
+STATICFILES_DIR = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # django-allauth config
 
