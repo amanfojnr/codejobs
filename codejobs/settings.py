e70aa2c0d5031981cd571c50753fa5b28f5dce07
amanfojnr <kenmarch@zoho.com>
2017-12-08 14:54:52 +0000

    fixed static root error

--- codejobs/settings.py
+++ codejobs/settings.py
@@ -132,13 +132,6 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files
-STATICFILES_DIR = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
 # django-allauth config
 
 ACCOUNT_EMAIL_VERIFICATION = None
